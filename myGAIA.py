c04f0a9aa8ff6655c27a4cb72213869f237614b9
yqz206@gmail.com <yqz206@gmail.com>
2019-03-06 15:22:43 +0800

    reorgnizing Gaia

--- myGAIA.py
+++ myGAIA.py
@@ -1,1 +1,15 @@
 
+import os
+
+import os.path
+import numpy as np
+import pywcsgrid2
+from matplotlib import rc
+from  mpl_toolkits.axes_grid1.axes_rgb import imshow_rgb
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes, mark_inset,inset_axes
+from numpy import mean, sqrt, square
+
+
+
+from subprocess import call 
+import img_scale 
